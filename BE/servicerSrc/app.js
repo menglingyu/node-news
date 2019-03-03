4ab259652a4e9507ebbdb6d6bdb9a9f9f72762b4
menglingyu <lingyu.meng@baishancloud.com>
2019-03-03 18:06:43 +0800

    跨域

--- BE/servicerSrc/app.js
+++ BE/servicerSrc/app.js
@@ -1,8 +1,8 @@
 const Koa = require('koa');
 // const bodyParser = require('koa-bodyparser');
 import mongooseStartFun from './mongodb'
-// import routers from './routers'
-// const routers = require('./routers')
+const cors = require("koa2-cors");
+
 const router = require('./routers/index')
 
 const _PORT = 3000 // 服务端端口号
@@ -12,6 +12,24 @@ const app = new Koa();
 // app.use(bodyParser());  // 解析request的body
 
 mongooseStartFun()  // 运行mongodb
+
+app.use(
+  cors({
+    origin: function(ctx) {
+      return "*"
+
+      // if (ctx.url === "/test") {
+      //   return "*"; // 允许来自所有域名请求
+      // }
+      // return "http://localhost:8080"; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+    },
+    exposeHeaders: ["WWW-Authenticate", "Server-Authorization"],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ["GET", "POST", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization", "Accept"]
+  })
+);
 
 // app.use(routers.routes()).use(routers.allowedMethods()) // 初始化路由中间件
 app.use(router.routes()).use(router.allowedMethods())
--- BE/servicerSrc/routers/api.js
+++ BE/servicerSrc/routers/api.js
@@ -1,12 +1,14 @@
 const router = require('koa-router')()
 
-router.get('/get/data.json', (ctx) => {
+router.get('/car', (ctx) => {
+    // ctx.;
     ctx.body = {
-        success: true,
-        data: {
-            text: '我是api'
-        }
-    }
+      success: true,
+      data: {
+        list: ["汽车之家新闻1", "汽车之家新闻2", "汽车之家新闻3"]
+      }
+      // text: '我是api
+    };
 }).get('/get/user.json', (ctx) => {
     ctx.body = {
         success: true,
